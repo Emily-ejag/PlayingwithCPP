191c2341416e421a08df8a7e2b56d20032edca78
emily <emily.artegar@gmail.com>
2021-09-09 13:16:41 -0700

    cmath Library Lecture

---
+++ MathFun/MathFun/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+	cout << "pow(2, 5) = " << pow(2, 5) << endl;
+	cout << "sqrt(8) = " << sqrt(8) << endl;
+	cout << "ceil(4.2) = " << ceil(4.2) << endl;
+	cout << "floor(4.2) = " << floor(4.2) << endl;
+	cout << "log2(512) = " << log2(512) << endl;
+	return 0;
+}
